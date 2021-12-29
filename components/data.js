9d85ddabbb76d469462d4e6b92c41a05c4a19cd5
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-29 17:41:03 +0900

    calc_app

---
+++ components/data.js
@@ -0,0 +1,7 @@
+export default [
+  {"name":"taro", "mail":"taro@yamada", "age":39},
+  {"name":"hanako", "mail":"hanako@flower", "age":23},
+  {"name":"sachiko", "mail":"sachiko@happy", "age":17},
+  {"name":"jiro", "mail":"jiro@second", "age":6},
+  {"name":"mami", "mail":"mami@mumemo", "age":50}
+]
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,39 @@
-import {useState, useEffect} from 'react'
+import useSWR from 'swr'
+import {useState} from 'react'
 import Layout from '../components/layout'
 
 export default function Home() {
-  const url = './data.json'
-  const [data, setData] = useState({message:'', data:[]})
-  
-  useEffect(()=>{
-    fetch(url)
-      .then(res=> res.json())
-      .then(res=> setData(res))
-  }, [])
+  const [pref, setPref] = useState({id:0, item:'name'})
+  const [address, setAddress] = useState('/api/hello'
+    + pref.id + '/' + pref.item)
+  const { data, err } = useSWR(address)
+
+  const onChange = (e) => {
+    pref.id = e.target.value
+    setPref(pref)
+    setAddress('/api/hello/' + pref.id + '/' + pref.item)
+  }
+
+  const onSelect = (e) => {
+    pref.item = e.target.value
+    setPref(pref)
+    setAddress('/api/hello/' + pref.id + '/' + pref.item)
+  }
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className='alert alert-primary text-center'>
-          <h5 className='mb-4'>{data.message}</h5>
-          <table className='table bg-white'>
-            <thead className='table-dark'>
-              <tr>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.data.map((value, key)=>(
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <td>{value.mail}</td>
-                  <td>{value.age}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <h5 className='mb-4'>
+            { JSON.stringify(data)}
+          </h5>
+          <input type="number" className='form-control form-control-sm mb-2'
+            onChange={onChange} />
+          <select onChange={onSelect} className='form-control form-control-sm'>
+            <option value="name">Name</option>
+            <option value="mail">Mail</option>
+            <option value="age">Age</option>
+          </select>
         </div>
       </Layout>
     </div>
